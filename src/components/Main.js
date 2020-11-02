c4597b32081599e220fe8e369b852cda2adaf981
bruteforce1414 <bruteforce1414@gmail.com>
2020-11-02 17:54:42 +0500

    feat: form for github

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,21 +2,39 @@ import { Switch, Route } from 'react-router-dom';
 import Repos from './Repos';
 import Home from './Home';
 import Tag from './Tag';
+import React, { useState } from "react";
 
 
 
 
-const Main = () => (
-  <main>
+export default function Main () {
+  const [value, setValue] = useState("");
+
+  const handleChange = e => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    alert("you have searched for - " + value);
+    // or you can send to backend
+  };
+ 
+  return(
+  <div>
+  <form>
+        <input value={value} onChange={handleChange}/>
+        <button onClick={handleSubmit} type="submit">
+          Submit
+        </button>
+      </form>
     <Switch>
       <Route exact path='/' component={Home}/>
       <Route exact path='/:user/' component={Repos}/>
       <Route exact path='/:user/:repo' component={Tag}/>
   
 
-{/*       <Route exact path='/:repo/:tags' component={Tags}/>
- */}    </Switch>
-  </main>
-)
+   </Switch>
+   </div>
+)}
 
-export default Main;
