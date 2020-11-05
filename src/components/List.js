dcf00d59ff2caa6493e5bfca998f8bec3ed2e374
bruteforce1414 <bruteforce1414@gmail.com>
2020-11-05 17:12:16 +0500

    feat: list of repositories

--- src/components/List.js
+++ src/components/List.js
@@ -25,7 +25,18 @@ var repos;
   return (
     <div className="List">
       <h1>repos is</h1>
-      <h1>{pokemon}</h1>
+      {pokemon.map((repo) => {
+        return (
+          <li key={repo.id} className='list'>
+            <span className='repo-text'>{repo} </span>
+
+            <Link to={{
+              pathname: '/details',
+              search: "repository=" + repo
+            }}><span className='repo-description'>{repo}</span></Link>
+          </li>
+        );
+      })}
 {/*       <img alt={pokemon.name} src={pokemon.sprites.front_default} />
  */}    </div>
   );
