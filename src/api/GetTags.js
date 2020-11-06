27fe72a1ce7fa36fec0e87045845896dce049ac9
bruteforce1414 <bruteforce1414@gmail.com>
2020-11-06 13:03:05 +0500

    feat: tags get successfully

---
+++ src/api/GetTags.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+
+async function Get(user) {
+
+    const apiUrl = "https://api.github.com/repos"+user+"/tags";
+
+  try {
+    const response = await axios.get(apiUrl);
+    return response;
+  } catch (error) {
+    return (error);
+  }
+
+}
+
+async function GetTags(user) {
+
+  try {
+    let x = await Get(user)
+    return x.data
+  } catch (err) {
+    return err;
+  }
+}
+
+export {
+  GetTags
+};
--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,29 +1,48 @@
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { GetTags } from '../api/GetTags';
 
 function Tags  () {
 
     let location = useLocation();
+    console.log("location in Tags", location.pathname)
     var arrayOfStrings = (location.pathname).split("/");
     console.log("arrayOfStrings", arrayOfStrings)
 
+    const [isLoading, setLoading] = useState(true);
+  const [repos, setRepos] = useState([]);
+
+
+  useEffect(() => {
+
+    GetTags(location.pathname).then(response => {
+      console.log("response in tags",)
+      response.map((number) => setRepos(oldArray => [...oldArray, number.name]));
+
+      setLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <div className="List">Loading...</div>;
+  }
+
+
 return(
-    <div>
-        <li><Link to={"/"+arrayOfStrings[1]}>{"/"+arrayOfStrings[1]}</Link></li>
+    <div className="List">
+    <li><Link to={"/"+arrayOfStrings[1]}>{"/"+arrayOfStrings[1]}</Link></li>
 
-        <ul>
-            <li>
-                v1
+      <h1>Tags:</h1>
+      {repos.map((repo) => {
+        return (
+          <li key={repo.id} className='list'>         
+            <Link to={{
+              pathname: location.pathname+"/"+repo,
+            }}><span className='repo-description'>{repo}</span></Link>
           </li>
-
-            <li>
-                v2
-          </li>
-
-            <li>
-                v3
-          </li>
-        </ul>
+        );
+      })}
     </div>
 )
 }
